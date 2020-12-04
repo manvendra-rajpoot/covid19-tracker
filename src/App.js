d9ad051a09014d123a143eb77ed8777d1b9a91d5
manvendra264 <manvendra141986@gmail.com>
2020-12-04 18:53:27 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import { FormControl, MenuItem, Select, Card, CardContent } from '@material-ui/core';
 import InfoBox from './InfoBox';
-import Map from './Map';
+import Mapp from './Mapp';
 import Table from './Table';
+import LineGraph from './LineGraph';
+import { sortData } from './util';
+import 'leaflet/dist/leaflet.css' ;
 
 function App() {
   //variable in react
@@ -11,7 +14,11 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData,setTableData] = useState([]);
-  
+
+  const [mapCenter, setMapCenter] = useState({lat:34.80746, lng: -40.4796});
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+
   //useEffect() is a powerful `hook` in react to 
   //run a piece of  code based on a condition
   useEffect(() => {
@@ -28,9 +35,11 @@ function App() {
               value: country.countryInfo.iso2,
             }
           ));
-
+          
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
-          setTableData(data);
+          setMapCountries(data);
         });
     };
     getCountriesData();
@@ -59,9 +68,11 @@ function App() {
         setCountry(updated_countryCode);
         //all of countrydata
         setCountryInfo(data);
+        // map center
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
       });
   };
-  console.log("INFO------->", countryInfo);
+  // console.log("INFO------->", countryInfo);
 
   return (
     //bem class naming convention
@@ -71,34 +82,51 @@ function App() {
         
         <div className='app__header'>
           <h1>COVID-19 TRACKER</h1>
-          <FormControl className='app__header'>
-            <Select variant='outlined' 
-              onChange={onCountryChange}
-              value={country}>
+          <FormControl className='app__header__form'>
+            <Select variant='outlined' onChange={onCountryChange} value={country}>
+              
               <MenuItem value='worldwide'>WorldWide</MenuItem>
               {countries.map((country) => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
               ))}
+
             </Select>
           </FormControl>
         </div>
 
         <div className='app__stats'>
-          <InfoBox title='CoronaVirus Cases' cases={countryInfo.todayCases} total={countryInfo.cases} />
-          <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
-          <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths} /> 
+          <InfoBox title='CoronaVirus Cases' 
+            cases={countryInfo.todayCases} 
+            total={countryInfo.cases} />
+          <InfoBox title='Recovered' 
+            cases={countryInfo.todayRecovered} 
+            total={countryInfo.recovered} />
+          <InfoBox title='Deaths' 
+            cases={countryInfo.todayDeaths} 
+            total={countryInfo.deaths} /> 
         </div>
-        
-        <Map />
+         
+        <Mapp countries={mapCountries} center={mapCenter} zoom={mapZoom} />
+
       </div>
 
       <Card className='app__right'>
+        
         <CardContent>
-          <h3>Live Cases by Country</h3>
+          <h3>Live Cases By Country</h3>
           <Table countries={tableData} />
           
-          {/* Graphs */}
-          <h3>WorldWide new cases</h3>
+          <h3>Worldwide Overview </h3>
+
+          {/* <h6>Total Cases</h6> */}
+          <LineGraph />
+          
+          {/* <h6>Recovered</h6> */}
+          {/* <LineGraph caseType='recovered' bg_col='#66ff66' border_col='#009900' /> */}
+
+          {/* <h6>Deaths</h6> */}
+          {/* <LineGraph caseType='deaths' /> */}
+
         </CardContent>
 
       </Card>
